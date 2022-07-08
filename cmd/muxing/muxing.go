ecf0ce7ffec58db2d498dcb0cf17d5023bfc2ecd
Turaj <mehdiev.t@gmail.com>
2022-07-08 18:20:30 +0600

    tt

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +21,11 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/", mainPage)
+	router.HandleFunc("/bad", badPage).Methods("GET")
+	router.HandleFunc("/name/{param}", nameParamPage).Methods("GET")
+	router.HandleFunc("/data", dataPage).Methods("POST")
+	router.HandleFunc("/headers", headersPage).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -27,7 +33,7 @@ func Start(host string, port int) {
 	}
 }
 
-//main /** starts program, gets HOST:PORT param and calls Start func.
+// main /** starts program, gets HOST:PORT param and calls Start func.
 func main() {
 	host := os.Getenv("HOST")
 	port, err := strconv.Atoi(os.Getenv("PORT"))
@@ -35,4 +41,51 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func badPage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func nameParamPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.Write([]byte("Hello, " + vars["param"] + "!"))
+	w.WriteHeader(http.StatusOK)
+}
+
+func dataPage(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	w.Write([]byte("I got message:\n" + string(data)))
+}
+
+func headersPage(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	if a, ok := h["A"]; ok {
+		if b, ok := h["B"]; ok {
+			first, err := strconv.Atoi(a[0])
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			second, err := strconv.Atoi(b[0])
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			w.Header().Set("a+b", strconv.Itoa(first+second))
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
 }
